c6bfd8b83bd6f984a3d7f02c1551c5c7133d57de
Ajay <132923078+Ajayavi@users.noreply.github.com>
2025-07-13 21:16:45 +0530

    Add files via upload

---
+++ Day_1.py
@@ -0,0 +1,47 @@
+#Print Practice
+print("Ajay")
+print("CSE")
+print("VIT")
+print("I am learning Python for placements!")
+#Variable Practice
+reg_no="22MIC9870"
+cgpa=float(9.28)
+year=int(3)
+print(reg_no+" "+str(cgpa)+" "+str(year))
+#Data Types Practice
+i=int()
+j=float()
+k=str()
+l=bool()
+print(type(i))
+print(type(j))
+print(type(k))
+print(type(l))
+#Small Calculations
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+#Swpping
+print(a,b)
+a,b=b,a
+print(a,b)
+#Concatenate Strings
+fn="Pooja "
+ls="Ajay"
+fl=fn+ls
+print(fl)
+#Types and Conversion
+x=5
+y=float(x)
+z=str(y)
+print("x: "+str(type(x)))
+print("y: "+str(type(x)))
+print("z: "+str(type(x)))
+#Fahrenheit to Celsius
+fah=35
+cel=(fah-32)*5/9
+print(cel)
